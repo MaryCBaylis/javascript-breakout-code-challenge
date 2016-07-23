5b7811b9f730c004f9b6aa41a76d3e4fd91981f0
Mary Baylis <mary.c.baylis@gmail.com>
2016-07-22 21:46:47 -0400

    Add height and width attribute to brick

--- app/assets/javascripts/brick.js
+++ app/assets/javascripts/brick.js
@@ -1,6 +1,8 @@
 var Brick = function(brickParams){
 	this.x = brickParams.x;
 	this.y = brickParams.y;
+	this.width = data.brickWidth;
+	this.height = data.brickHeight;
 	this.color = brickParams.color;
 	this.fadeLevel = 1;
 }
@@ -10,27 +12,27 @@ Brick.prototype = (function(){
 		update: function(){
 
 		}, 
-		
+
 		draw: function(context){
 			//Draw color brick
 			context.fillStyle = "rgba(" + this.color + ", " + this.fadeLevel + ")";
-			context.fillRect(this.x, this.y, data.brickWidth, data.brickHeight)
+			context.fillRect(this.x, this.y, this.width, this.height)
 
 			//Add horizontal shadow
-			var shadow = context.createLinearGradient(this.x, 0, this.x + data.brickWidth, 0);
+			var shadow = context.createLinearGradient(this.x, 0, this.x + this.width, 0);
 			shadow.addColorStop(0, "rgba(0, 0, 0, " + this.fadeLevel * 0.4 + ")");
 			shadow.addColorStop(1, "rgba(255, 255, 255, 0)");
 
 			context.fillStyle = shadow;
-			context.fillRect(this.x, this.y, data.brickWidth, data.brickHeight);
+			context.fillRect(this.x, this.y, this.width, this.height);
 
 			//Add vertical shadow
-			shadow = context.createLinearGradient(0, this.y, 0, this.y + data.brickHeight);
+			shadow = context.createLinearGradient(0, this.y, 0, this.y + this.height);
 			shadow.addColorStop(0, "rgba(0, 0, 0, " + this.fadeLevel * 0.4 + ")");
 			shadow.addColorStop(1, "rgba(255, 255, 255, 0)");
 
 			context.fillStyle = shadow;
-			context.fillRect(this.x, this.y, data.brickWidth, data.brickHeight);
+			context.fillRect(this.x, this.y, this.width, this.height);
 		}
 	}
 }());
